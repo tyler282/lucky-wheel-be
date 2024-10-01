cdd762a6e9019b181ad6ad9d7a57cbb260ae846b
vvhien17 <hien.vo@xpodc.com>
2024-10-01 11:48:15 +0700

    validator payload helper

--- src/common/response-message.ts
+++ src/common/response-message.ts
@@ -4,3 +4,8 @@ export enum ResponseMessage {
   NOT_FOUND = 'Not Found',
   INVALID_CREDENTIALS = 'Invalid Credentials',
 }
+
+export enum ErrorMessage {
+  IS_REQUIRED = 'is required',
+  BAD_REQUEST = 'Bad Request',
+}
