c26cb9073ecbcfa777141d32d5d2c541d7de0903
vvhien17 <hien.vo@xpodc.com>
2024-10-01 14:08:35 +0700

    missing import module redeem gift

--- src/redeem-gift/redeem-gift.module.ts
+++ src/redeem-gift/redeem-gift.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RedeemGiftService } from './redeem-gift.service';
 import { RedeemGiftController } from './redeem-gift.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RedeemGift } from './entities/redeem-gift.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RedeemGift])],
   controllers: [RedeemGiftController],
   providers: [RedeemGiftService],
 })
