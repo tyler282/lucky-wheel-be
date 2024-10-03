4565fb4c92531e534faab5b93c1e4ced7e9e8541
Ben <bao.nguyen@xpodc.com>
2024-10-03 15:20:34 +0700

    add feature create redeem point history

--- src/redeem-point-history/dto/create-redeem-point-history.dto.ts
+++ src/redeem-point-history/dto/create-redeem-point-history.dto.ts
@@ -1,1 +1,9 @@
-export class CreateRedeemPointHistoryDto {}
+import { ApiProperty } from '@nestjs/swagger';
+export class CreateRedeemPointHistoryDto {
+    @ApiProperty({})
+    userId: number;
+    @ApiProperty({})
+    point: number;
+    @ApiProperty({})
+    redeemGiftId: number;
+}
